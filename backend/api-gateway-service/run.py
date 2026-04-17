3c3a9dad451c2fe03a34d2ab1292af975a3fd5f3
TonyDamov <antoan.damov@gmail.com>
2026-04-17 16:18:37 +0300

    Update api-gateway
    made it in debug mode when on a local machine but not restart when ci runs a smoke test.

--- backend/api-gateway-service/run.py
+++ backend/api-gateway-service/run.py
@@ -6,6 +6,15 @@ load_dotenv()  # loads root .env
 
 app = create_app()
 
+
 if __name__ == "__main__":
+    debug = os.getenv("FLASK_DEBUG", "false").lower() == "true"
     port = int(os.environ.get("API_GATEWAY_PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=debug,
+        use_reloader=debug  # important!
+    )
+
