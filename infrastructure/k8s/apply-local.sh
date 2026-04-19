a97a73c27d7468c361be9f46d5208d49caa7a4d3
TonyDamov <antoan.damov@gmail.com>
2026-04-19 14:20:58 +0300

    Removed trailing slash

--- infrastructure/k8s/apply-local.sh
+++ infrastructure/k8s/apply-local.sh
@@ -1,19 +1,19 @@
 set -e
 
 echo "==> Applying manifests..."
-kubectl apply -f /infrastructure/k8s/namespace.yaml
-kubectl apply -f /infrastructure/k8s/configmap.yaml
-kubectl apply -f /infrastructure/k8s/secret.yaml 
-kubectl apply -f /infrastructure/k8s/backend/api-gateway-service/service.yaml
-kubectl apply -f /infrastructure/k8s/backend/api-gateway-service/deployment.yaml
-kubectl apply -f /infrastructure/k8s/backend/auth-service/service.yaml
-kubectl apply -f /infrastructure/k8s/backend/auth-service/deployment.yaml
-kubectl apply -f /infrastructure/k8s/backend/review-service/service.yaml
-kubectl apply -f /infrastructure/k8s/backend/review-service/deployment.yaml
-kubectl apply -f /infrastructure/k8s/backend/trail-service/service.yaml
-kubectl apply -f /infrastructure/k8s/backend/trail-service/deployment.yaml
-kubectl apply -f /infrastructure/k8s/frontend/service.yaml
-kubectl apply -f /infrastructure/k8s/frontend/deployment.yaml
+kubectl apply -f infrastructure/k8s/namespace.yaml
+kubectl apply -f infrastructure/k8s/configmap.yaml
+kubectl apply -f infrastructure/k8s/secret.yaml 
+kubectl apply -f infrastructure/k8s/backend/api-gateway-service/service.yaml
+kubectl apply -f infrastructure/k8s/backend/api-gateway-service/deployment.yaml
+kubectl apply -f infrastructure/k8s/backend/auth-service/service.yaml
+kubectl apply -f infrastructure/k8s/backend/auth-service/deployment.yaml
+kubectl apply -f infrastructure/k8s/backend/review-service/service.yaml
+kubectl apply -f infrastructure/k8s/backend/review-service/deployment.yaml
+kubectl apply -f infrastructure/k8s/backend/trail-service/service.yaml
+kubectl apply -f infrastructure/k8s/backend/trail-service/deployment.yaml
+kubectl apply -f infrastructure/k8s/frontend/service.yaml
+kubectl apply -f infrastructure/k8s/frontend/deployment.yaml
 
 echo ""
 echo "    Frontend:    http://localhost:80"
